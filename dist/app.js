09f559718ecfcbb6306e600fe74d30657ea74f6a
Stephen <steve.wiggins7@gmail.com>
2016-10-13 01:18:22 -0400

    Images on the screen

--- dist/app.js
+++ dist/app.js
@@ -8,11 +8,22 @@ var Game;
         }
         wwwFrogger.prototype.preload = function () {
             //graphics
-            this.game.load.image("tile", "/Graphics/TitleScreen.png");
+            this.game.load.image("background", "Graphics/backg.png");
+            this.game.load.image("frogUp", "Graphics/frogUp.png");
+            // this.game.load.image("background", "Graphics/backg.png");
+            // this.game.load.image("background", "Graphics/backg.png");
             //spritesheets
             //audio
         };
         wwwFrogger.prototype.create = function () {
+            var backg = this.game.cache.getImage("background");
+            var frogUp = this.game.cache.getImage("frogUp");
+            console.log(frogUp.height);
+            console.log(frogUp.width);
+            this.backgroundSprite = this.game.add.sprite(0, 0, "background");
+            this.backgroundSprite = this.game.add.sprite(0, 500, "frogUp");
+            // this.backgroundSprite = this.game.add.sprite(0,0,"background");
+            // this.backgroundSprite = this.game.add.sprite(0,0,"background");
         };
         return wwwFrogger;
     }());
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ module Game {
     export class wwwFrogger {
         
         game:Phaser.Game;
+        backgroundSprite: Phaser.Sprite;
+        frogUpSprite: Phaser.Sprite;
 
         constructor() {
             
@@ -14,15 +16,27 @@ module Game {
 
         preload(){
             //graphics
-            this.game.load.image("background", "/Graphics/backg.png");
+            this.game.load.image("background", "Graphics/backg.png");
+            this.game.load.image("frogUp", "Graphics/frogUp.png");
+            // this.game.load.image("background", "Graphics/backg.png");
+            // this.game.load.image("background", "Graphics/backg.png");
+            
             //spritesheets
-
+            
             //audio
 
         }
 
         create(){
-
+            var backg = this.game.cache.getImage("background");
+            var frogUp = this.game.cache.getImage("frogUp");            
+            console.log(frogUp.height);
+            console.log(frogUp.width);            
+            this.backgroundSprite = this.game.add.sprite(0,0,"background");
+            this.backgroundSprite = this.game.add.sprite(0,500,"frogUp");
+            // this.backgroundSprite = this.game.add.sprite(0,0,"background");
+            // this.backgroundSprite = this.game.add.sprite(0,0,"background");
+            
         }
     }
 
